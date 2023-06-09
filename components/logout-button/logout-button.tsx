f85f991e82151e6d39357ea9bb895b0bcf9d56a7
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-06-09 21:44:26 +0530

    feat: :sparkles: add remove profile

--- components/logout-button/logout-button.tsx
+++ components/logout-button/logout-button.tsx
@@ -1,14 +1,17 @@
 import { Pressable, Text } from "react-native";
 import { styles } from "./styles";
 import { useSetRecoilState } from "recoil";
-import { authAtom } from "atoms";
+import { authAtom, userAtom } from "atoms";
 import { deleteItemAsync } from "expo-secure-store";
+import { User } from "types/models";
 
 const LogoutButton = () => {
 	const setIsAuthenticated = useSetRecoilState<boolean>(authAtom);
+	const setUser = useSetRecoilState<User>(userAtom);
 
 	const onLogoutClick = async () => {
 		await deleteItemAsync("auth-token");
+		setUser({});
 		setIsAuthenticated(false);
 	};
 
